821befd174fa75760689a16037a283117f1d2a89
marufsiyam91 <marufsiyam9123@gmail.com>
2025-01-01 20:37:56 +0600

    tailwind file configure for better styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,8 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'esm': '500px',
+        'sm': '640px',
+        'md': '768px',
+        'lmd': '880px',
+        'lg': '1024px',
+        'pxl': '1200px',
+        'xl': '1280px',
+        '2xl': '1370px',
+        '4xl': '1536px'
+      },
+    },
   },
+  darkMode: 'class', // Correct spelling
   plugins: [],
 }
 
