086bd42d6e1e79d10a7f3397d4d6d089484ddc76
marufsiyam91 <marufsiyam9123@gmail.com>
2025-01-06 23:37:03 +0600

    add update blog and delete blog codes.

--- Backend/controllers/blog.controller.js
+++ Backend/controllers/blog.controller.js
@@ -54,4 +54,37 @@ const getBlog = async (req, res) => {
     }
 }
 
+const updateBlog = async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndUpdate(req.params.id, req.body);
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
+          }
+      
+          res.status(200).send(blog);
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+const deleteBlog = async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndDelete(req.params.id)
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
+          }
+      
+          res.status(204).json({message: 'blog deleted successfully'});
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
 
